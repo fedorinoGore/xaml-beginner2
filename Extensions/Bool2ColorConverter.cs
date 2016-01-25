c4d9e6d5bda100237763425c0653eed84a0dba89
fedorinoGore <fredy26@mail.ru>
2016-01-25 19:21:30 +0300

    Module Homework done...

---
+++ Extensions/Bool2ColorConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+
+namespace RestaurantManager.Extensions
+{
+    public class Bool2ColorConverter : IValueConverter
+    {
+        public string TrueColor { get; set; }
+        public string FalseColor { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var defaultColor = "Transparent";
+            if (value is bool)
+            {
+                //return (bool)value ? Colors.LightSalmon : Colors.Transparent;
+                return (bool)value ? this.TrueColor: this.FalseColor;
+            }
+            return defaultColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if(value is Color)
+            {
+                return (Color)value == Colors.LightSalmon ? true : false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Extensions/RightClickBehavior.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace RestaurantManager.Extensions
+{
+    public class RightClickBehavior : DependencyObject, IBehavior
+    {
+        public DependencyObject AssociatedObject { get; private set; }
+        public string Message { get; set; } = "God Bless you";
+        public string Title { get; set; }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            if (associatedObject is Page) {
+                this.AssociatedObject = associatedObject;
+                (this.AssociatedObject as Page).RightTapped += Right_Click;
+            }
+        }
+
+        private async void Right_Click(object sender, RightTappedRoutedEventArgs e)
+        {
+            await new MessageDialog(this.Message, this.Title).ShowAsync();
+        }
+
+        public void Detach()
+        {
+            (this.AssociatedObject as Page).RightTapped -= Right_Click;
+        }
+    }
+}
