38fe9d80fd92f3579a652b5f1bf33ba7494b42b7
fedorinoGore <fredy26@mail.ru>
2016-02-16 02:11:06 +0300

    Added ViewModels project, implemented ContextFactory for MVVM model refactoring

---
+++ RestaurantManager.ViewModels/ViewModel.cs
@@ -0,0 +1,38 @@
+﻿using RestaurantManager.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManager.ViewModels
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        protected RestaurantContext Repository { get; private set; }
+
+        public ViewModel()
+        {
+            LoadData();
+        }
+
+        private async void LoadData()
+        {
+            this.Repository = await RestaurantContextFactory.GetRestaurantContextAsync();
+            onDataLoaded();
+        }
+
+        protected abstract void onDataLoaded();
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void FirePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
